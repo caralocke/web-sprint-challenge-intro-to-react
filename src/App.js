6f7377deed12aedbdb6da9b4c3e7ebdf6dab749e
caralocke <cara.m.locke@gmail.com>
2021-06-11 09:40:39 -0400

    Used useEffect and axios to fetch the information obtained from the given API each time the DOM renders.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Character from './components/Character'
 
@@ -6,15 +7,32 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [characters, setCharacters] = useState([])
-  const [currentCharacter, setCurrentCharacter] = useState('')
+  const [currentCharacter, setCurrentCharacter] = useState(null)
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+    .get(`https://swapi.dev/api/people`)
+    .then(res => {
+      console.log(`App.js res.data`, res.data)
+      setCharacters(res.data)
+    })
+    .catch(err => {
+      console.log(`Here's where you messed up: \n`, err)
+    })
+  },[])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map((character) => {
+          return <Character key={character.id} character={character} />
+        })
+      }
     </div>
   );
 }
