8ac074ef8a9c6710cb1b9eac63a435eb22b9cd10
caralocke <cara.m.locke@gmail.com>
2021-06-11 11:20:48 -0400

    Imported ThemeProvider to index.js as well as created a theme object and imported it to index.js. Also wrapped ThemeProvider around App and provided my theme object as a prop so it can be accessed anywhere in App.js

---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: 'burlywood',
+    secondaryColor: 'darkgoldenrod',
+    tertiaryColor:'tan',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: '(max-width: 550px)',
+}
