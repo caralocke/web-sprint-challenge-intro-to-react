0974de2cd35e997db0b54ffcdea27ef2f18e958f
caralocke <cara.m.locke@gmail.com>
2021-06-11 10:02:14 -0400

    Was able to get character's names to render to the screen.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,11 @@
 // Write your Character component here
 import React from 'react'
 
-export default function Character() {
+export default function Character(props) {
+    const {character} = props
     return (
         <div>
-            
+            {character.name}            
         </div>
     )
 }
